0cf39d24c72eeec0175feca3a51260019ee29ac3
namuk <ngug1234@naver.com>
2015-01-29 01:12:19 +0900

    source

---
+++ example/Servomotor.ino
@@ -0,0 +1,36 @@
+#include "Tlc5940.h"
+#include "tlc_servos.h"
+
+int numberOfServos = 9; //how many servos on the chain?
+
+
+void setup(){
+  tlc_initServos();  // Note: this will drop the PWM freqency down to 50Hz.
+}
+
+void loop(){
+  //loop through all the servos and move one at a time to 180º
+  for(int i = 0; i<numberOfServos; i++){
+
+    for (int angle = 0; angle < 180; angle+= 10) {
+      tlc_setServo(i, angle);
+      Tlc.update();
+      delay(20);
+    }
+
+    delay(200);
+  }
+
+  //loop through all the servos and move one at a time to 0º
+  for(int i = 0; i<numberOfServos; i++){
+
+    for (int angle = 180; angle >= 0; angle-= 10) {
+      tlc_setServo(i, angle);
+      Tlc.update();
+      delay(20);
+    }
+
+    delay(200);
+  }
+}
+
